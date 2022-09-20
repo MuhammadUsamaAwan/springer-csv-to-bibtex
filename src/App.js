6f78d9734e0bdea36ee8bc94de6046119ecc77fc
Muhammad Usama <underusama@gmail.com>
2022-09-20 08:02:30 -0700

    ready to deploy

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import Papa from 'papaparse'
+import axios from 'axios'
+import { saveAs } from 'file-saver'
 
-function App() {
+const App = () => {
+  const [inputLength, setInputLength] = useState(0)
+  const [outputLength, setOutputLength] = useState(0)
+  const [loading, setLoading] = useState(false)
+
+  const handleChange = async e => {
+    setLoading(true)
+    const file = e.target.files[0]
+    if (file.type !== 'text/csv') {
+      alert('Please upload a CSV File!')
+      return
+    }
+    Papa.parse(file, {
+      header: true,
+      skipEmptyLines: true,
+      complete: async results => {
+        const responseData = []
+        setInputLength(results?.data.length)
+        for (let i = 0; i < results?.data?.length; i++) {
+          try {
+            const res = await axios.get(
+              `http://dx.doi.org/${results?.data[i]['Item DOI']}`,
+              {
+                headers: {
+                  Accept: 'text/bibliography; style=bibtex',
+                },
+              }
+            )
+            responseData.push(res?.data)
+            setOutputLength(prev => prev + 1)
+          } catch (err) {
+            console.error(err)
+          }
+        }
+        const blob = new Blob(responseData, {
+          type: 'text/plain;charset=utf-8',
+        })
+        saveAs(blob, 'springer_citations.bib')
+        setLoading(false)
+        setInputLength(0)
+        setOutputLength(0)
+      },
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <section className='grid justify-center mt-20'>
+      <h1 className='text-center text-3xl mb-10 font-semibold'>
+        Springer SCV To BibTex
+      </h1>
+      {loading ? (
+        <h3 className='text-center'>
+          Loading... This might take a while depending on your input file
+          (Progress: {outputLength}/{inputLength})
+        </h3>
+      ) : (
+        <input type='file' name='file' accept='.csv' onChange={handleChange} />
+      )}
+    </section>
+  )
 }
 
-export default App;
+export default App
